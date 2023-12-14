e627d52b75993493b25d545480bb2f1dda2f978d
Shahjahan Sarfraz <110630702+Jahan-Shah@users.noreply.github.com>
2023-12-14 21:02:07 +0500

    authentication & sidebar

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Spinner from "@/components/spinner";
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+import Navigation from "./_components/navigation";
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  if (isLoading) {
+    return (
+      <div className="flex h-full items-center justify-center">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="dark:bg=[#1f1f1f] flex h-full">
+      <Navigation />
+      <main className="h-full flex-1 overflow-y-auto">{children}</main>
+    </div>
+  );
+};
+
+export default MainLayout;
--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,10 +1,16 @@
 "use client";
 import { ModeToggle } from "@/components/mode-toggle";
+import Spinner from "@/components/spinner";
+import { Button } from "@/components/ui/button";
 import useScrollTop from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
+import { SignInButton, SignUpButton, UserButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
+import Link from "next/link";
 import Logo from "./logo";
 
 const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop(50);
   return (
     <div
@@ -15,6 +21,27 @@ const Navbar = () => {
     >
       <Logo />
       <div className="flex w-full items-center justify-between gap-x-2 md:ml-auto md:justify-end">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignUpButton mode="modal">
+              <Button size="sm">Get Jotion Free</Button>
+            </SignUpButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Jotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
