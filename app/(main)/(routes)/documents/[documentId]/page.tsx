64c1c7b13ffe4f8d4ce278c07d19193de1e097c3
Shahjahan Sarfraz <110630702+Jahan-Shah@users.noreply.github.com>
2024-01-24 20:52:55 +0500

    :sparkles: Added Blocknote RTE

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
 
 import { Cover } from "@/components/cover";
 import { Toolbar } from "@/components/toolbar";
+import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 
@@ -14,15 +17,41 @@ interface DocumentIdPageProps {
 }
 
 const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const Editor = useMemo(
+    () => dynamic(() => import("@/components/editor"), { ssr: false }),
+    [],
+  );
+
   const document = useQuery(api.documents.getById, {
     documentId: params.documentId,
   });
 
+  const update = useMutation(api.documents.update);
+
+  const onChange = (content: string) => {
+    update({
+      id: params.documentId,
+      content,
+    });
+  };
+
   if (document === undefined) {
-    return <div>Loading..</div>;
+    return (
+      <div>
+        <Cover.Skeleton />
+        <div className="mx-auto mt-10 md:max-w-3xl lg:max-w-4xl">
+          <div className="space-y-4 pl-8 pt-4">
+            <Skeleton className="h-14 w-1/2" />
+            <Skeleton className="h-4 w-4/5" />
+            <Skeleton className="h-4 w-2/5" />
+            <Skeleton className="h-4 w-3/5" />
+          </div>
+        </div>
+      </div>
+    );
   }
   if (document === null) {
-    return <div>not found</div>;
+    return <div>Not found</div>;
   }
 
   return (
@@ -30,6 +59,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
       <Cover url={document.coverImg} />
       <div className="mx-auto md:max-w-3xl lg:max-w-4xl">
         <Toolbar initialData={document} />
+        <Editor onChange={onChange} initialContent={document.content} />
       </div>
     </div>
   );
